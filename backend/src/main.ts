cf8ae34d602f7e90279eeefc07c0bc3b77bdda10
phuong quyen <phuongquyen.huynh@aquariux.com>
2025-06-22 01:07:54 +0700

    fix cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
 
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Zigvy Interview API')
     .setVersion('1.0')
@@ -22,7 +29,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 4000);
 }
 
 bootstrap();
